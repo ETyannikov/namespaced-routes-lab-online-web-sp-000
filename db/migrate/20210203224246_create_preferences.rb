0ab3d220c34baef2812e1818572d983f5c7e3a9d
Eddy Tyannikov <etyannikov@gmail.com>
2021-02-03 22:48:09 +0000

    Automatically backed up by Learn

--- db/migrate/20210203224246_create_preferences.rb
+++ db/migrate/20210203224246_create_preferences.rb
@@ -1,7 +1,8 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.bool :
+      t.bool :allow_create_artists
+      t.bool :allow_create_songs
     end
   end
 end
